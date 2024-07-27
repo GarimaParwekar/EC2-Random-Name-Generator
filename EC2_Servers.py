83afeeedd735111649a7336b0b8e95e3051dab00
GarimaParwekar <parwekar.garima@gmail.com>
2024-07-27 02:39:22 -0400

    Create EC2_Servers.py

---
+++ EC2_Servers.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+No_of_EC2_Instances = int(input('Please enter how many EC2 instances you want? '))
+Department_Name = input('Enter your Department name ')
+while True:
+    for i in range(No_of_EC2_Instances):
+        Random_Character = random.choices(string.ascii_letters)
+        Random_Number = random.randint(0,9)
+        Random_EC2_Instance_Name = Department_Name + Random_Character+str(Random_Number)
+        print('Name of the EC2 instance is' , Random_EC2_Instance_Name)
+    break 
+
+
+        
+
