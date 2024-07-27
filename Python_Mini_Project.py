7bf5caed8f49cf2f340a37cb60fc58de664cc751
GarimaParwekar <parwekar.garima@gmail.com>
2024-07-27 06:55:52 -0700

    Update Python_Mini_Project.py

--- Python_Mini_Project.py
+++ Python_Mini_Project.py
@@ -2,10 +2,9 @@ import random
 import string
 No_of_EC2_Instances = int(input('Please enter how many EC2 instances you want: '))
 Department_Name = input('Enter your Department name: ')
-while True:
-    for i in range(No_of_EC2_Instances):
-        Random_Character = ''.join(random.choices(string.ascii_letters, k=3))
-        Random_Number = ''.join(random.choices(string.digits,k=3))
-        Random_EC2_Instance_Name = Department_Name + '-' + Random_Character + str(Random_Number)
-        print( Random_EC2_Instance_Name)
-    break
+for i in range(No_of_EC2_Instances):
+    Random_Character = ''.join(random.choices(string.ascii_letters, k=3))
+    Random_Number = ''.join(random.choices(string.digits,k=3))
+    Random_EC2_Instance_Name = Department_Name + '-' + Random_Character + str(Random_Number)
+    print( Random_EC2_Instance_Name)
+    
